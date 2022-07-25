e3056747cf3a37fb8c5cbb277caf51eb2a14caf4
Jaime Leon <jaime.leon@globant.com>
2022-07-25 12:17:55 -0500

    AccessibilitySandbox

---
+++ Examples/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Jaime Yesid Leon Parada on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
